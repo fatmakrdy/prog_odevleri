c0d77366e8c46186b3e1cdc7c235bf5472d132d3
= <=>
2022-10-05 21:48:15 +0300

    first commit

---
+++ src/saat_ödevi/main.java
@@ -0,0 +1,19 @@
+package saat_ödevi;
+
+public class main {
+
+	public static void main(String[] args) {
+	
+		for(int i=12;i<=23;i++) {
+		
+	    for(int j=0;j<=59;j++) {
+		   
+	    	System.out.println(i+":"+j);
+	   }
+	    
+	    }System.out.println("00:00");
+		
+	
+	}
+	
+}
